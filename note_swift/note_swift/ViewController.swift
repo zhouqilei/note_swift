dd65e4a23809cb469e00da0356e9ef49fce99a03
周启磊 <zhouql@sunwayworld.com>
2020-10-10 15:09:23 +0800

    初始化项目的基本架构

---
+++ note_swift/note_swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  note_swift
+//
+//  Created by 周启磊 on 2020/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
